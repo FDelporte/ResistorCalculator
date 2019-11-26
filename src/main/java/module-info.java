c13c9b8ae9a48cd257dd90f85149dc6fec3a18dc
Frank Delporte <f.delporte@televic.com>
2019-11-26 19:30:07 +0100

    0.0.3 correct module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module be.webtechie.piheaders {
+module be.webtechie.resistorcalculator {
     exports be.webtechie.resistorcalculator;
     exports be.webtechie.resistorcalculator.definition;
     exports be.webtechie.resistorcalculator.util;
